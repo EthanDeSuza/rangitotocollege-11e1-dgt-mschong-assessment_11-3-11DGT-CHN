67594a8f0f14ca369f2244f44bf55e3d52fda6ea
EthanDeSuza <177944@cloud.rangitoto.school.nz>
2025-08-18 12:40:13 +1200

    More tkinter practice for the assessment on Monday 18th August 2025

--- Python/DGT11.3.py
+++ Python/DGT11.3.py
@@ -7,14 +7,14 @@ stuff for doing the tkinter assessment
 import tkinter
 m = tkinter.Tk()
 m.mainloop()
-
+""""""
 # This code creates a simple Tkinter window with a label.
 from tkinter import *
 root = Tk()
 w = Label(root, text="Silksong Tomorrow!")
 w.pack()
 root.mainloop()
-
+""""""
 # This code creates a Tkinter window with a button that closes the window when clicked.
 import tkinter as tk
 r = tk.Tk()
@@ -22,7 +22,7 @@ r.title('Silksong release date')
 button = tk.Button(r, text='Now', width=25, command=r.destroy)
 button.pack()
 r.mainloop()
-
+""""""
 # This code creates a Tkinter window with two text entry fields for first name and last name.
 from tkinter import *
 master = Tk()
@@ -33,7 +33,7 @@ e2 = Entry(master)
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 mainloop()
-
+""""""
 # This code creates a Tkinter window with two checkboxes for selecting options.
 from tkinter import *
 master = Tk()
@@ -42,7 +42,7 @@ Checkbutton(master, text='Believer', variable=var1).grid(row=0, sticky=W)
 var2 = IntVar()
 Checkbutton(master, text='Denier', variable=var2).grid(row=1, sticky=W)
 mainloop()
-
+""""""
 # This code creates a Tkinter window with two radio buttons for selecting a character.
 from tkinter import *
 root = Tk()
@@ -50,7 +50,7 @@ v = IntVar()
 Radiobutton(root, text='Champion of Fools', variable=v, value=1).pack(anchor=W)
 Radiobutton(root, text='Invincible, Fearless, Sensual, Mysterious, Enchanting, Vigorous, Diligent, Overwhelming, Gorgeous, Passionate, Terrifying, Beautiful, Powerful Grey Prince Zote', variable=v, value=2).pack(anchor=W)
 mainloop()
-
+""""""
 # This code creates a Tkinter window with a listbox containing titles.
 from tkinter import *
 top = Tk()
@@ -63,7 +63,7 @@ Lb.insert(5, 'The White Lady')
 Lb.insert(6, 'The Radiance')
 Lb.pack()
 top.mainloop()
-
+""""""
 # This code creates a Tkinter window with a scrollbar and a listbox that can be scrolled.
 from tkinter import *
 root = Tk()
@@ -76,7 +76,7 @@ for line in range(100):
 mylist.pack(side=LEFT, fill=BOTH)
 scrollbar.config(command=mylist.yview)
 mainloop()
-
+""""""
 # This code creates a Tkinter window with a menu bar containing File and Help menus.
 from tkinter import *
 root = Tk()
@@ -92,7 +92,7 @@ helpmenu = Menu(menu)
 menu.add_cascade(label='Help', menu=helpmenu)
 helpmenu.add_command(label='About')
 mainloop()
-
+""""""
 # This code creates a Tkinter window with a combobox for selecting a game title.
 import tkinter as tk
 from tkinter import ttk
@@ -117,7 +117,7 @@ combo_box.set("Select a game")
 # Bind event to section
 combo_box.bind("<<ComboboxSelected>>", select)
 root.mainloop()
-
+""""""
 # This code creates a Tkinter window with two scales: one vertical and one horizontal.
 from tkinter import *
 master = Tk()
@@ -126,7 +126,7 @@ w.pack()
 w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
 w.pack()
 mainloop()
-
+""""""
 # This code creates a Tkinter window with a top-level window titled "Silksong".
 from tkinter import *
 root = Tk()
@@ -134,3 +134,79 @@ root.title('Silksong release date')
 top = Toplevel()
 top.title('Silksong')
 top.mainloop()
+""""""
+# This code creates a Tkinter window with a message widget displaying a message.
+from tkinter import *
+main = Tk()
+ourMessage = 'Silksong comes out tomorrow!'
+messageVar = Message(main, text=ourMessage)
+messageVar.config(bg='lightgreen')
+messageVar.pack()
+main.mainloop()
+""""""
+# This code creates a Tkinter window with a menubutton containing two checkbuttons.
+from tkinter import *
+top = Tk()
+mb = Menubutton(top, text = "Silksong")
+mb.grid()
+mb.menu = Menu(mb, tearoff = 0)
+mb["menu"] = mb.menu
+cVar = IntVar()
+aVar = IntVar()
+mb.menu.add_checkbutton(label = 'Champion of Fools', variable = cVar)
+mb.menu.add_checkbutton(label = 'Invincible, Fearless, Sensual, Mysterious, Enchanting, Vigorous, Diligent, Overwhelming, Gorgeous, Passionate, Terrifying, Beautiful, Powerful Grey Prince Zote', variable = aVar)
+mb.pack()
+top.mainloop()
+""""""
+# This code creates a Tkinter window with a progress bar that simulates a download process.
+import tkinter as tk
+from tkinter import ttk
+import time
+
+def start_progress():
+    progress.start()
+
+    # simulate a task that takes time to complete
+    for i in range(101):
+        # Simulate some work
+        time.sleep(0.05)
+        progress['Value'] = i
+        # Update the GUI
+        root.update_idletasks()
+    progress.stop()
+
+root = tk.Tk()
+root.title("Silksong Download Progress")
+# Create a progressbar widget
+progress = ttk.Progressbar(root, orient="horizontal", length=500, mode="determinate")
+progress.pack(pady=20)
+# Button to start the progress
+start_button = tk.Button(root, text="Start Download", command=start_progress)
+start_button.pack(pady=10)
+root.mainloop()
+""""""
+# This code creates a Tkinter window with a spinbox for selecting a number.
+from tkinter import *   
+master = Tk()
+w = Spinbox(master, from_=0, to=10)
+w.pack()
+mainloop()
+""""""
+# This code creates a Tkinter window with a text widget that displays multiple lines of text.
+from tkinter import *
+root = Tk()
+T = Text(root, height=2, width=30)
+T.pack()
+T.insert(END, 'Silksong comes out tomorrow!\nNUH UH!\n\nYUH HUH!\n')
+mainloop()
+""""""
+# This code creates a Tkinter window with a canvas that draws a horizontal line.
+from tkinter import *
+master = Tk()
+w = Canvas(master, width=40, height=60)
+w.pack()
+canvas_height=20
+canvas_width=200
+y = int(canvas_height / 2)
+w.create_line(0, y, canvas_width, y )
+mainloop()
