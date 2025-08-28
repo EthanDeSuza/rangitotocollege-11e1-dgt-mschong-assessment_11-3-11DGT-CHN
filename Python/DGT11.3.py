42015a5f90f34fe3335c742afa57c5216bf8e1be
EthanDeSuza <177944@cloud.rangitoto.school.nz>
2025-08-28 14:39:07 +1200

    Update DGT11.3.py
    uploading work from 27/08/2025

--- Python/DGT11.3.py
+++ Python/DGT11.3.py
@@ -6,27 +6,48 @@ stuff for doing the tkinter assessment
 
 import tkinter
 
+def wordle_start():
+    root = tk.Tk()
+    root.title('Wordle')
+    root.geometry('400x600')
+    words = []
+def blackjack_start():
+    root = tk.Tk()
+    root.title('BlackJack')
+    root.geometry('800x600') 
+def snake_start():
+    root = tk.Tk()
+    root.title('Snake')
+    root.geometry('600x600') 
+def scoreboard_start():
+    root = tk.Tk()
+    root.title('Scoreboard')
+    root.geometry('600x500')
+
+
 # This code creates a Tkinter window with a menu bar containing File and Help menus.
-from tkinter import *
-root = Tk()
+import tkinter as tk
+root = tk.Tk()
 root.title('Games Compendium')
-menu = Menu(root)
+root.geometry('800x450')
+menu = tk.Menu(root)
 root.config(menu=menu)
-filemenu = Menu(menu)
+filemenu = tk.Menu(menu)
 menu.add_cascade(label='Navigation', menu=filemenu)
-filemenu.add_command(label='Scoreboard')
+filemenu.add_command(label='Scoreboard', command=scoreboard_start)
 filemenu.add_separator()
 filemenu.add_command(label='Exit', command=root.destroy)
-helpmenu = Menu(menu)
+helpmenu = tk.Menu(menu)
 menu.add_cascade(label='Help', menu=helpmenu)
 helpmenu.add_command(label='Game 1 Instructions')
 helpmenu.add_command(label='Game 2 Instructions')
 helpmenu.add_command(label='Game 3 Instructions')
-button = Button(root, text='Now', width=25, command=root.destroy)
-button.grid(row=20, column=0, padx=50, pady=50)
-button = Button(root, text='Now', width=25, command=root.destroy)
-button.grid(row=20, column=20, padx=50, pady=50)
-button = Button(root, text='Now', width=25, command=root.destroy)
-button.grid(row=20, column=40, padx=50, pady=50)
-mainloop()
 
+
+button = tk.Button(root, text='Wordle', width=10, command=wordle_start)
+button.grid(row=20, column=0, padx=90, pady=50)
+button = tk.Button(root, text='BlackJack', width=10, command=blackjack_start)
+button.grid(row=20, column=20, padx=90, pady=50)
+button = tk.Button(root, text='Snake', width=10, command=snake_start)
+button.grid(row=20, column=40, padx=90, pady=50)
+tk.mainloop()
